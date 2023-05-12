3fed1ad15faa7c841753b572cd51e631ba642a69
Sumit <sumitp950@gmail.com>
2023-05-12 19:42:48 +0530

    ofice

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
 
-function App() {
+export default function App() {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    const url = "https://fakestoreapi.com/products";
+
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url);
+        const json = await response.json();
+        setUsers(json);
+        console.log(json);
+      } catch (error) {
+        console.log("error", error);
+      }
+    };
+
+    fetchData();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        {users.length > 0 && (
+           <div className="container">
+           <div className="row">
+         
+            {users.map((user, i) => (
+              <>
+               
+                    <div className="col-md-4">
+                    <h2>{user.category}</h2>
+                    <div class="card" key={i}>
+                      <img
+                        src={user.image}
+                        alt="Avatar"
+                        style={{ width: "10%" }}
+                      />
+                      <div class="">
+                        <h4>
+                          <b>{user.title}</b>
+                        </h4>
+                        <p>{users.description}</p>
+                      </div>
+                    
+                </div></div>
+              </>
+            ))}
+         
+          </div>
+                  </div>
+        )}
+      </div>
+    </>
   );
 }
-
-export default App;
